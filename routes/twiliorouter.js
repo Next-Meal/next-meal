a3fa5acba64a5c333cb32bfdba2d2f76e95d1cbd
David Michael Zalk <dzknj@outlook.com>
2016-05-02 15:54:58 -0700

    fixed linting errors

--- routes/twiliorouter.js
+++ routes/twiliorouter.js
@@ -1,17 +1,15 @@
+/* eslint-disable camelcase */
+
 const Router = require('express').Router;
-const bodyParser = require('body-parser').urlencoded({
-  extended: false
-});
-const errorHandler = require(__dirname + '/../lib/error_handler');
 const location = require(__dirname + '/../models/location');
-const twilio = require('twilio');
 
 var TwimlResponse = require('twilio').TwimlResponse;
 var twilioRouter = module.exports = exports = Router();
 
 twilioRouter.get('/sms', (req, res) => {
-  console.log(req.query.Body)
-  if(!(req.query.Body === 'Breakfast' || req.query.Body === 'Lunch' || req.query.Body === 'Dinner')) {
+  console.log(req.query.Body);
+  if (!(req.query.Body === 'Breakfast' || req.query.Body ===
+  'Lunch' || req.query.Body === 'Dinner')) {
     console.log('User Entered incorrect meal parameter');
     var resp = new TwimlResponse();
     resp.message('Next Meal\n You Must Enter Either:\nBreakfast\nLunch\nor\nDinner');
@@ -19,8 +17,8 @@ twilioRouter.get('/sms', (req, res) => {
     console.log(resp.toString());
     res.send(resp.toString());
   } else {
-  location.find({meal_served: req.query.Body }, (err, data) => {
-    if(!err) console.log(data);
+    location.find({ meal_served: req.query.Body }, (err, data) => {
+      if (!err) console.log(data);
       var smsBody = req.query.Body;
       var smsNumber = req.query.From;
       var smsCity = req.query.FromCity;
@@ -34,10 +32,14 @@ twilioRouter.get('/sms', (req, res) => {
       var str = data;
       console.log('\nLINE SPLIT\n' + str[1] + '\n2ND LINE SPLIT\n' + str[2]);
       var resp = new TwimlResponse();
-      resp.message('Next-Meal\n' + str[0].meal_served + '\n' + str[0].location + '\n' + str[0].day_time + '\n' + str[0].people_served + '\n' + str[1].meal_served + '\n' + str[1].location + '\n' + str[1].day_time + '\n' + str[1].people_served + '\n' + str[2].meal_served + '\n' + str[2].location + '\n' + str[2].day_time + '\n' + str[2].people_served + '\n');
+      resp.message('Next-Meal\n' + str[0].meal_served + '\n' + str[0].location + '\n'
+       + str[0].day_time + '\n' + str[0].people_served + '\n' + str[1].meal_served + '\n'
+        + str[1].location + '\n' + str[1].day_time + '\n' + str[1].people_served + '\n'
+         + str[2].meal_served + '\n' + str[2].location + '\n' + str[2].day_time + '\n'
+          + str[2].people_served + '\n');
       res.type('text/xml');
       console.log(resp.toString());
       res.send(resp.toString());
     });
-  };
+  }
 });
