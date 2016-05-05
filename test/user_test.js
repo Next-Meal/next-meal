7c881796af51b24a087e143f7006a089bb91b131
David Michael Zalk <dzknj@outlook.com>
2016-05-05 11:15:41 -0700

    fixed before and after block in user test

--- test/user_test.js
+++ test/user_test.js
@@ -8,12 +8,18 @@ const server = require(__dirname + '/../_server');
 
 describe('user router', () => {
   before((done) => {
-    this.PORT = process.env.PORT;
-    this.MONGODB_URI = 'mongodb://localhost/next_meal_test';
-    this.server = server(this.PORT, this.MONGODB_URI, done);
+    this.portBackup = process.env.PORT;
+    this.mongoUriBackup = process.env.MONGODB_URI;
+    this.PORT = process.env.PORT = 5000;
+    this.MONGODB_URI = process.env.MONGODB_URI = 'mongodb://localhost/next_meal_test';
+    this.server = server(this.PORT, this.MONGODB_URI, () => {
+      setTimeout(done, 1000);
+    });
   });
 
   after((done) => {
+    process.env.PORT = this.portBackup;
+    process.env.MONGODB_URI = this.mongoUriBackup;
     mongoose.connection.db.dropDatabase(() => {
       mongoose.disconnect(() => {
         this.server.close(done);
