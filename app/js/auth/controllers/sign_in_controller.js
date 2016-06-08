ed4b083b92db454c411b57e0dc6c95468c72eea6
David Michael Zalk <dzknj@outlook.com>
2016-06-08 11:58:48 -0700

    merging from upstream

---
+++ app/js/auth/controllers/sign_in_controller.js
@@ -0,0 +1,23 @@
+var baseUrl = require('../../config').baseUrl;
+module.exports = function(app) {
+  app.controller('SignInController',
+  ['$http', '$location', 'handleError', 'nmAuth',
+  function($http, $location, handleErr, auth) {
+    this.buttonText = 'Sign in to your Organization';
+    this.errors = [];
+    this.authenticate = function(organization) {
+      $http({
+        method: 'GET',
+        url: baseUrl + '/api/signin',
+        headers: {
+          'Authorization': 'Basic ' + window.btoa(organization.organizationName + ':' + organization.password)
+        }
+      })
+      .then((res) => {
+        auth.saveToken(res.data.token);
+        auth.getOrganizationName();
+        $location.path('/org_locations');
+      }, handleErr(this.errors, 'could not sign into organization'));
+    };
+  }]);
+};
--- app/js/auth/controllers/sign_up_controller.js
+++ app/js/auth/controllers/sign_up_controller.js
@@ -1,16 +1,17 @@
 var baseUrl = require('../../config').baseUrl;
 module.exports = function(app) {
-  app.controller('SignUpController', ['$http', '$location', 'nmAuth', function($http, $location, auth) { /* TODO: ADD errorHandler service!!! */
+  app.controller('SignUpController', ['$http', '$location', 'handleError', 'nmAuth',
+  function($http, $location, handleErr, auth) {
     this.signup = true;
-    // this.errors = [];  TODO: add errorHandler service
+    this.errors = [];
     this.buttonText = 'Create New Organization';
     this.authenticate = function(organization) {
       $http.post(baseUrl + '/api/signup', organization)
       .then((res) => {
         auth.saveToken(res.data.token);
         auth.getOrganizationName();
-        $location.path('/org_locations') // TODO: path is not correct, fix this!! Finish org router!!
-      }, /* TODO: ADD handleError service here! */ );
+        $location.path('/org_locations');
+      }, handleErr(this.errors, 'could not create user'));
     };
   }]);
 };
