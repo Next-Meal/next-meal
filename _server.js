4122172fd037d114287ff21efaaa9476fb8953b6
Mark Buchthal <mark.buchthal@gmail.com>
2016-06-08 13:18:13 -0700

    add list styles

--- _server.js
+++ _server.js
@@ -6,6 +6,13 @@ const twilioRouter = require(__dirname + '/routes/twilio_router');
 const userRouter = require(__dirname + '/routes/user_router');
 const voiceRouter = require(__dirname + '/routes/voice_router');
 
+app.use((req, res, next) => {
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization, token');
+  res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
+  next();
+});
+
 app.use('/voice', voiceRouter);
 app.use('/api', userRouter);
 app.use('/api', twilioRouter);
