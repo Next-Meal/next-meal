df987e7a6beeb905e149f239405225aa390803a1
Mark Buchthal <mark.buchthal@gmail.com>
2016-06-09 19:37:09 -0700

    organizational form

--- app/js/auth/controllers/sign_up_controller.js
+++ app/js/auth/controllers/sign_up_controller.js
@@ -10,7 +10,7 @@ module.exports = function(app) {
       .then((res) => {
         auth.saveToken(res.data.token);
         auth.getOrganizationName();
-        $location.path('/organization_meals');
+        // $location.path('#/');
       }, handleErr(this.errors, 'could not create user'));
     };
   }]);
